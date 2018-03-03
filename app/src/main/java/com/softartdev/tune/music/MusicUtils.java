7a9f762912be5a35e3dda463b0c9de6b6960c05d
artur <artik222012@gmail.com>
2018-03-04 02:10:36 +0500

    media playback activity

--- app/src/main/java/com/softartdev/tune/music/MusicUtils.java
+++ app/src/main/java/com/softartdev/tune/music/MusicUtils.java
@@ -16,6 +16,8 @@
 
 package com.softartdev.tune.music;
 
+import android.content.Context;
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.support.v4.media.MediaMetadataCompat;
 import android.support.v4.media.session.MediaControllerCompat;
@@ -24,8 +26,12 @@ import android.view.View;
 import android.widget.TextView;
 
 import com.softartdev.tune.R;
+import com.softartdev.tune.ui.main.music.MediaPlaybackActivity;
 
 import org.jetbrains.annotations.NotNull;
+
+import java.util.Formatter;
+import java.util.Locale;
 
 /*
  * Static methods useful for activities
@@ -56,18 +62,45 @@ public class MusicUtils {
                 }
                 artist.setText(artistName);
                 nowPlayingView.setVisibility(View.VISIBLE);
-/*
                 nowPlayingView.setOnClickListener(new View.OnClickListener() {
                     public void onClick(View v) {
                         Context c = v.getContext();
                         c.startActivity(new Intent(c, MediaPlaybackActivity.class));
                     }
                 });
-*/
                 return;
             }
         }
         nowPlayingView.setVisibility(View.GONE);
+    }
+
+    /*  Try to use String.format() as little as possible, because it creates a
+     *  new Formatter every time you call it, which is very inefficient.
+     *  Reusing an existing Formatter more than tripled the speed of
+     *  makeTimeString().
+     *  This Formatter/StringBuilder are also used by makeAlbumSongsLabel()
+     */
+    private static StringBuilder sFormatBuilder = new StringBuilder();
+    private static Formatter sFormatter = new Formatter(sFormatBuilder, Locale.getDefault());
+    private static final Object[] sTimeArgs = new Object[5];
+
+    public static String makeTimeString(Context context, long secs) {
+        String durationformat = context.getString(
+                secs < 3600 ? R.string.durationformatshort : R.string.durationformatlong);
+
+        /* Provide multiple arguments so the format can be changed easily
+         * by modifying the xml.
+         */
+        sFormatBuilder.setLength(0);
+
+        final Object[] timeArgs = sTimeArgs;
+        timeArgs[0] = secs / 3600;
+        timeArgs[1] = secs / 60;
+        timeArgs[2] = (secs / 60) % 60;
+        timeArgs[3] = secs;
+        timeArgs[4] = secs % 60;
+
+        return sFormatter.format(durationformat, timeArgs).toString();
     }
 
 }
